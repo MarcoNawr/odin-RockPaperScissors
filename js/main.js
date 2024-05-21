21cb44021b29a2cc3100d78a89e5a87d5aa335d2
Marco <166357601+MarcoNawr@users.noreply.github.com>
2024-05-21 22:54:41 +0200

    feature: rock paper scissors logic created

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function getComputerChoice(){
     //random return “rock”, “paper” or “scissors”
     let choice = "";
     let random = Math.random();
-    console.log("Random Number = " + random);
+    //console.log("Random Number = " + random);
 
     if (random < 0.33){
         return choice = "rock";
@@ -39,12 +39,63 @@ function playRound(){
     let computerChoice = getComputerChoice();
     let winner = "";
 
-    if (humanChoice === "errorMessage"){
-        return "Error: use rock, paper or scissors"
-    } else {// TBD AUSWERTUNGSLOGIK WER HAT GEWONNEN
-    }
-    
+    // if (humanChoice === "errorMessage"){
+    //     return "Error: use rock, paper or scissors"
+    // } else {// TBD AUSWERTUNGSLOGIK WER HAT GEWONNEN
+    // }
+
+    switch (humanChoice) {
+        case 'errorMessage':
+            console.log("Falsche Eingabe. Erlaubt sind nur Rock, Paper oder Scissors")
+            break;
+        case 'rock':
+            if(computerChoice === "rock"){
+                winner="Rock vs. Rock = Unentschieden"
+            }else if(computerChoice === "paper"){
+                winner="Rock vs. Paper = Computer Wins";
+                computerScore += 1;
+            } else if (computerChoice === "scissors"){
+                winner="Rock vs. Scissors = Human Wins";
+                humanScore += 1;
+            }
+            break;  
+        case 'paper':
+            if(computerChoice === "rock"){
+                winner="Paper vs. Rock = Human Wins"
+                humanScore += 1;
+            }else if(computerChoice === "paper"){
+                winner="Paper vs. Paper = Unentschieden";
+            } else if (computerChoice === "scissors"){
+                winner="Paper vs. Scissors = Computer Wins";
+                computerScore += 1;
+            }
+            break;  
+        case 'scissors':
+            if(computerChoice === "rock"){
+                winner="Scissors vs. Rock = Computer Wins"
+                computerScore += 1;
+            }else if(computerChoice === "paper"){
+                winner="Scissors vs. Paper = Human Wins";
+                humanScore += 1;
+            } else if (computerChoice === "scissors"){
+                winner="Scissors vs. Scissors = Unentschieden";
+            }
+            break;  
+        default:
+          console.log("Sorry, something went wrong");
+      }
+    console.clear();
+    console.log(winner + " Zwischenstand ==> Human: " + humanScore + " Computer: " + computerScore);
+    playAgain();
 }
+
+function playAgain(){
+    let again = prompt("Nochmal? y/n: ");
+    if(again === "y"){
+        playRound();
+    }else{}
+}
+
 
 // INVOKE FUNKTIONS 
 //#############################
