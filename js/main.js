283b31e0af9334be80e07acfb12389c6abb0bbaf
Marco <166357601+MarcoNawr@users.noreply.github.com>
2024-06-19 21:31:22 +0200

    style: changes in the resultdisplay

--- js/main.js
+++ js/main.js
@@ -26,6 +26,17 @@ const SVG_ROCK = '<svg class="svgRock" xmlns="http://www.w3.org/2000/svg" viewBo
 const SVG_PAPER = '<svg class="svgPaper" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2024 Fonticons, Inc. --><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V240c0 8.8-7.2 16-16 16s-16-7.2-16-16V64c0-17.7-14.3-32-32-32s-32 14.3-32 32V336c0 1.5 0 3.1 .1 4.6L67.6 283c-16-15.2-41.3-14.6-56.6 1.4s-14.6 41.3 1.4 56.6L124.8 448c43.1 41.1 100.4 64 160 64H304c97.2 0 176-78.8 176-176V128c0-17.7-14.3-32-32-32s-32 14.3-32 32V240c0 8.8-7.2 16-16 16s-16-7.2-16-16V64c0-17.7-14.3-32-32-32s-32 14.3-32 32V240c0 8.8-7.2 16-16 16s-16-7.2-16-16V32z"/></svg>';
 const SVG_SCISSOR = '<svg class="svgScissors" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2024 Fonticons, Inc. --><path d="M40 208c-22.1 0-40 17.9-40 40s17.9 40 40 40l180.2 0c-7.6 8.5-12.2 19.7-12.2 32c0 25.3 19.5 46 44.3 47.9c-7.7 8.5-12.3 19.8-12.3 32.1c0 26.5 21.5 48 48 48l32 0 64 0c70.7 0 128-57.3 128-128l0-113.1c0-40.2-16-78.8-44.4-107.3C444.8 76.8 413.9 64 381.7 64L336 64c-21.3 0-39.3 13.9-45.6 33.1l74.5 23.7c8.4 2.7 13.1 11.7 10.4 20.1s-11.7 13.1-20.1 10.4L288 129.9l0 .1L84 65.8C62.9 59.2 40.5 70.9 33.8 92s5.1 43.5 26.2 50.2L269.5 208 40 208z"/></svg>';
 
+//Animation
+const resultScaling = [
+    { transform: "rotate(0) scale(1)" },
+    { transform: "rotate(2deg) scale(1)" },
+  ];
+  
+  const resultTiming = {
+    duration: 400,
+    iterations: 1,
+  };
+
 // Event Listeners
 //#############################
 
@@ -214,6 +225,7 @@ function playRound(){
     console.clear();
     console.log(winner + " Zwischenstand ==> Human: " + humanScore + " Computer: " + computerScore);
     p_rpsResultGame.textContent = `Human:  ${humanScore} Computer: ${computerScore}`;
+    p_rpsResultCurrentRound.animate(resultScaling, resultTiming);
 }
 
 function setRpsResultCurrentRound(text, backgroundColor, color){
